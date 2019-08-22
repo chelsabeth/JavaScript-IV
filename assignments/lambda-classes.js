ce8fa1f5c47d43c867148be80c26ed309f444dcc
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-08-22 16:58:22 -0400

    instructor and student done on lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,6 +1,8 @@
 // CODE here for your Lambda Classes
 
 
+//PERSON
+
 class Person {
     constructor(attributes){
         this.name = attributes.name,
@@ -8,14 +10,69 @@ class Person {
         this.location = attributes.location
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}.`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
     }
 }
+
+//INSTRUCTOR
+
+class Instructor extends Person {
+    constructor(moreAttributes){
+        super(moreAttributes);
+        this.specialty = moreAttributes.specialty,
+        this.favLanguage = moreAttributes.favLanguage,
+        this.catchPhrase = moreAttributes.catchPhrase
+    }
+    demo(subject) {
+        return `Today we are leaning about ${subject}`
+    }
+    grade() {
+        return `${student.name} recieves a perfect score on ${subject}.`;
+    }
+}
+
+//STUDENT
+
+class Student extends Instructor {
+    constructor(studentAttributes){
+        super(studentAttributes);
+        this.perviousBackground = studentAttributes.perviousBackground,
+        this.className = studentAttributes.className,
+        this.favSubject = studentAttributes.favSubject
+    }
+    listSubjects() {
+        `My favorite subjects are ${this.favSubject}, ${this.favSubject}, and ${this.favSubject}.`;
+    }
+    PRAssignment() {
+        `${student.name} has submitted a PR for ${subject}.`;
+    }
+    sprintChallenge() {
+        `${student.name} has begun sprint challenge on ${subject}.`
+    }
+}
+
+
+
+
+
 
 const fred = new Person({
     name: 'Fred',
     age: 'stone age',
     location: 'Bedrock',
 })
+
+const instructor = new Instructor({
+    name: 'Sandy',
+    age: 28,
+    location: 'California',
+    specialty: 'being kind',
+    favLanguage: 'Swift',
+    catchPhrase: 'knowledge is power!',
+})
+
+
+
+
 
 console.log(fred.speak());
